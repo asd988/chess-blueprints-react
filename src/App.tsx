e2d586b8affcd25738241891df257a58c71240a7
asd988 <enderhd.98@gmail.com>
2022-11-21 16:52:03 +0100

    style

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import { CreateGame } from './pages/CreateGame';
 
 const App = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={ <CreateGame/> }/>
+    <Routes>
+      <Route path='/' element={<CreateGame />} />
 
-        <Route path='*' element={<p>404</p>}/>
-      </Routes>
-    </div>
+      <Route path='*' element={<p>404</p>} />
+    </Routes>
   );
 }
 
--- src/pages/CreateGame.tsx
+++ src/pages/CreateGame.tsx
@@ -1,7 +1,7 @@
 
 export const CreateGame = () => {
   return (
-    <div>
+    <div className="w-1/3 rounded-2xl bg-[#0003] p-4">
       <p>amogus</p>
     </div>
   )
